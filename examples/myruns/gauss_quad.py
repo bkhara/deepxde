d55a281d7ce76e60304fc4b5ec3c32e6dbd35a46
Biswajit <biswajitkhara@gmail.com>
2022-09-10 02:43:25 -0500

    Added element based Gaussian quadrature in a mesh

--- examples/myruns/gauss_quad.py
+++ examples/myruns/gauss_quad.py
@@ -23,9 +23,9 @@ class GaussQuadrature():
 
 	def create_GQ_scheme(self):
 		if self.dim == 1:
-			self.gp, self.gpw, self.J = self.create_GQ_scheme_1d()
+			self.gpx, self.gpw, self.J = self.create_GQ_scheme_1d()
 		elif self.dim == 2:
-			self.gp, self.gpw, self.J = self.create_GQ_scheme_2d()
+			self.gpx, self.gpy, self.gpw, self.J = self.create_GQ_scheme_2d()
 
 	def create_GQ_scheme_base(self, xRange, numpt):
 		res = gleg.leggauss(numpt)
@@ -51,20 +51,106 @@ class GaussQuadrature():
 		gp = np.stack((np.ravel(XX),np.ravel(YY)),1)
 		gpw = np.ravel(WW)
 		J = Jx*Jy
-		return gp, gpw, J
+		return gp[:,0], gp[:,1], gpw, J
 
 	def integrate(self, f_eval, f_integrand):
-		f = f_eval(self.gp)
+		if self.dim == 1:
+			f = f_eval(self.gpx)
+		elif self.dim == 2:
+			f = f_eval(self.gpx, self.gpy)
 		integrand = f_integrand(f)
 		gpsum = self.gpw * integrand
 		integral = np.sum(gpsum) * self.J
 		return integral
 
 	def calc_l2_distance(self, f_eval_1, f_eval_2):
-		f1 = f_eval_1(self.gp)
-		f2 = f_eval_2(self.gp)
+		if self.dim == 1:
+			f1 = f_eval_1(self.gpx)
+			f2 = f_eval_2(self.gpx)
+		elif self.dim == 2:
+			f1 = f_eval_1(self.gpx, self.gpy)
+			f2 = f_eval_2(self.gpx, self.gpy)
 		integrand = (f1 - f2)**2
 		gpsum = self.gpw * integrand
 		integral = np.sum(gpsum) * self.J
 		return np.sqrt(integral)
 
+
+class GaussQuadratureOnMesh():
+	"""docstring for GaussQuadratureOnMesh"""
+	def __init__(self, dim, domain=([0.,1.],[0.,1.]), nodes=(11,11), numpt=(2,2)):
+		super(GaussQuadratureOnMesh, self).__init__()
+		self.dim = dim
+		self.xRange = domain[0]
+		self.nodesx = nodes[0]
+		self.nelmx = self.nodesx - 1
+		self.numptx = numpt[0]
+		if self.dim >= 2:
+			self.yRange = domain[1]
+			self.nodesy = nodes[0]
+			self.nelmy = self.nodesy - 1
+			self.numpty = numpt[1]
+		self.create_GQ_mesh_2d()
+
+	def gen_new_scheme(self, numpt=(10,10)):
+		self.numptx = numpt[0]
+		if self.dim >= 2:
+			self.numpty = numpt[1]
+		self.create_GQ_mesh_2d()
+
+
+	def create_GQ_mesh_base(self, range1d, nodes, numpt):
+		nel = nodes - 1
+		h = (range1d[1] - range1d[0]) / nel
+
+		x = np.linspace(range1d[0], range1d[1], nodes, endpoint=True)
+		xl = x[:-1]
+
+		gquad = GaussQuadrature(1, domain=([0., h],), numpt=(numpt,))
+		gp = gquad.gpx
+		gpw = gquad.gpw
+
+		gp_grid = xl[:, np.newaxis] + gp[np.newaxis, :]
+
+		J = h/2.
+
+		return gp_grid, gpw, J
+
+	def create_GQ_mesh_2d(self):
+		gpX, gpwX, Jx = self.create_GQ_mesh_base(self.xRange, self.nodesx, self.numptx)
+		gpY, gpwY, Jy = self.create_GQ_mesh_base(self.yRange, self.nodesy, self.numpty)
+
+		self.gpx = np.zeros((self.numpty, self.numptx, self.nelmy, self.nelmx))
+		self.gpy = np.zeros((self.numpty, self.numptx, self.nelmy, self.nelmx))
+		self.gpw = np.zeros((self.numpty, self.numptx, self.nelmy, self.nelmx))
+
+		for j in range(self.numpty):
+			for i in range(self.numptx):
+				self.gpx[j, i, :, :] = np.tile(gpX[0:,i], (self.nelmy,1))
+				self.gpy[j, i, :, :] = (np.tile(gpY[0:,j], (self.nelmx,1))).T
+				self.gpw[j, i, :, :] = (gpwY[j] * gpwX[i])
+
+		self.J = Jx * Jy
+
+		# print("gpx = \n", self.gpx)
+		# print("gpy = \n", self.gpy)
+		# print("gpwX = \n", gpwX)
+		# print("gpwY = \n", gpwY)
+		# print("gpw = \n", self.gpw)
+
+	def integrate(self, f_eval, f_integrand):
+		f = f_eval(self.gpx, self.gpy)
+		integrand = f_integrand(f)
+		temp = self.gpw * integrand * self.J
+		gpsum = np.sum(np.sum(temp, axis=0), axis=1)
+		integral = np.sum(gpsum)
+		return integral
+
+	def calc_l2_distance(self, f_eval_1, f_eval_2):
+		f1 = f_eval_1(self.gpx, self.gpy)
+		f2 = f_eval_2(self.gpx, self.gpy)
+		integrand = (f1 - f2)**2
+		temp = self.gpw * integrand * self.J
+		gpsum = np.sum(np.sum(temp, axis=0), axis=1)
+		integral = np.sum(gpsum)
+		return np.sqrt(integral)
--- examples/myruns/test_gaussquadmesh.py
+++ examples/myruns/test_gaussquadmesh.py
@@ -1,25 +1,27 @@
 import unittest
 import numpy as np
-from gauss_quad import GaussQuadrature
+from gauss_quad import GaussQuadratureOnMesh
 
-class TestGaussQuadrature(unittest.TestCase):
-	"""docstring for TestGaussQuadrature"""
+class TestGaussQuadratureOnMesh(unittest.TestCase):
+	"""docstring for TestGaussQuadratureOnMesh"""
 	# def __init__(self):
-	# 	super(TestGaussQuadrature, self).__init__()
+	# 	super(TestGaussQuadratureOnMesh, self).__init__()
 
 	def test_integrate(self):
-		gquad = GaussQuadrature(2, domain=([0.,1.],[0.,1.]), numpt=(5,5))
+		Nx = Ny = 16
+		gquad = GaussQuadratureOnMesh(2, domain=([0.,1.],[0.,1.]), nodes=(Nx,Ny), numpt=(2,2))
 
 		def f1(x,y): return np.sin(np.pi*x)**2*np.cos(np.pi*y)**2
-		gquad.gen_new_scheme(numpt=(10,10))
+		# gquad.gen_new_scheme(numpt=(10,10))
 		val_c = gquad.integrate(f1, lambda x: x)
 		val_a = 0.25
-		# print("val_c = ", val_c, "val_a = ", val_a, "diff = {:.3e}".format(abs(val_c-val_a)))
+		print("val_c = ", val_c, "val_a = ", val_a, "diff = {:.3e}".format(abs(val_c-val_a)))
 		self.assertTrue(abs(val_c-val_a) < 1e-12)
 
 	def test_calc_l2_distance_1(self):
-		gquad = GaussQuadrature(2, domain=([0.,1.],[0.,1.]), numpt=(5,5))
-		
+		Nx = Ny = 64
+		gquad = GaussQuadratureOnMesh(2, domain=([0.,1.],[0.,1.]), nodes=(Nx,Ny), numpt=(3,3))
+
 		def f1(x,y): return x*y
 		def f2(x,y): return x**2*y**2
 		val_c = gquad.calc_l2_distance(f1, f2)
@@ -29,7 +31,7 @@ class TestGaussQuadrature(unittest.TestCase):
 
 		def f1(x,y): return np.sin(np.pi*x)
 		def f2(x,y): return np.cos(np.pi*x)
-		gquad.gen_new_scheme(numpt=(10,10))
+		# gquad.gen_new_scheme(numpt=(10,10))
 		val_c = gquad.calc_l2_distance(f1, f2)
 		val_a = np.sqrt(1)
 		print("val_c = ", val_c, "val_a = ", val_a, "diff = {:.3e}".format(abs(val_c-val_a)))
@@ -37,7 +39,7 @@ class TestGaussQuadrature(unittest.TestCase):
 
 		def f1(x,y): return np.sin(np.pi*x)*np.sin(np.pi*y)
 		def f2(x,y): return np.cos(np.pi*x)*np.cos(np.pi*y)
-		gquad.gen_new_scheme(numpt=(10,10))
+		# gquad.gen_new_scheme(numpt=(10,10))
 		val_c = gquad.calc_l2_distance(f1, f2)
 		val_a = np.sqrt(0.5)
 		print("val_c = ", val_c, "val_a = ", val_a, "diff = {:.3e}".format(abs(val_c-val_a)))
@@ -45,7 +47,7 @@ class TestGaussQuadrature(unittest.TestCase):
 
 		def f1(x,y): return np.sin(np.pi*x)*np.sin(np.pi*y)
 		def f2(x,y): return np.exp(x)*np.exp(y)
-		gquad.gen_new_scheme(numpt=(10,10))
+		# gquad.gen_new_scheme(numpt=(10,10))
 		val_c = gquad.calc_l2_distance(f1, f2)
 		val_a = np.sqrt(8.145142998121553314)
 		print("val_c = ", val_c, "val_a = ", val_a, "diff = {:.3e}".format(abs(val_c-val_a)))
